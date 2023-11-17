15fb3b632b420a35780f1b59cbdd80343ced66a5
CorreDump <147637057+CorreDump@users.noreply.github.com>
2023-11-17 20:57:49 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -5,31 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 64, 39, 22)),
+            ColorScheme.fromSeed(seedColor: Color.fromRGBO(07, 67, 49, 86)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,16 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -57,17 +32,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   int _selectedIndex = 0;
 
@@ -79,42 +43,97 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-         backgroundColor: Color.fromRGBO(7, 67, 49, 86),
+        backgroundColor: Color.fromRGBO(07, 67, 49, 86),
         centerTitle: true,
-
-        title: Image.asset(
-            "./images/logo.png"), // Use Image.asset to load an image from the assets folder
-
         leading: IconButton(
-            icon: Icon(
-              Icons.menu,
-            ),
+          icon: const Icon(
+            Icons.menu,
+          ),
+          color: Colors.white,
+          onPressed: () {
+            // Menu icon is pressed
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            color: Colors.white,
             onPressed: () {
-              // Menü simgesine tıklandığında yapılacak işlemler
+              // Notification icon is pressed
             },
           ),
-        actions: [
-            IconButton(
-              icon: Icon(Icons.notifications),
-              onPressed: () {
-                // Bildirim simgesine tıklandığında yapılacak işlemler
-              },
+        ],
+        title: Image.asset(
+          "./images/logo.png",
+          height: 30,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Events',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 20.0, // Set your desired font size
+                color: Color.fromRGBO(0, 0, 0, 0.667),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    child: Image.asset(
+                      './images/image_1.jpeg',
+                      fit: BoxFit.cover,
+                    ),
+                    height: 200,
+                    width: 100,
+                    decoration: BoxDecoration(color: Colors.amber),
+                    // Other content related to the first container
+                  ),
+                  SizedBox(
+                    height: 25,
+                    width: 50,
+                  ), // Add more containers as needed
+                  Container(
+                    child: Image.asset(
+                      './images/image_2.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(color: Colors.amber),
+                    // Other content related to the second container
+                  ),
+                  SizedBox(
+                    height: 25,
+                    width: 50,
+                  ),
+                  Container(
+                    child: Image.asset(
+                      './images/image_3.jpeg',
+                      fit: BoxFit.cover,
+                    ),
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(color: Colors.amber),
+                    // Other content related to the second container
+                  ),
+                ],
+              ),
             ),
           ],
-          
+        ),
       ),
-       bottomNavigationBar: BottomAppBar(
-        color: Color.fromRGBO(07, 67, 49, 86),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: Color.fromRGBO(173, 231, 214, 0.667),
         height: 70,
-        shape: const CircularNotchedRectangle(),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
@@ -133,7 +152,7 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             IconButton(
-              icon: Icon(Icons.panorama_horizontal_select),
+              icon: Icon(Icons.search),
               color: Colors.white,
               onPressed: () {
                 _onItemTapped(2);
@@ -148,12 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
